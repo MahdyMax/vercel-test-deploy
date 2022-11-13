62eed812fdb98abf27452849aba91fbc3d835424
Mahdymax <mahdyupwork@gmail.com>
2022-11-13 21:26:35 +0330

    commit fixed

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const sendEmail = require('./public/js/app');
 
 const PORT = process.env.PORT || 5000;
 
+
 // For ( sendgrid )
 app.use(express.urlencoded({ extended: false}));
 //
@@ -19,6 +20,7 @@ app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/images', express.static(__dirname + 'public/images'))
 app.use(express.json())
+
 //
 
 // Set views - engine

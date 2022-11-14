5f708a2ab59e4798ee16490902068c5179cb613c
Mahdymax <mahdyupwork@gmail.com>
2022-11-14 18:30:54 +0330

    static healthy check

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ app.use(express.urlencoded({ extended: false}));
 //
 
 // Middleware
-app.use(express.static('public'));
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/images', express.static(__dirname + 'public/images'))
+app.use(express.static('/public'));
+app.use('/css', express.static(__dirname + '/public/css'))
+app.use('/js', express.static(__dirname + '/public/js'))
+app.use('/images', express.static(__dirname + '/public/images'))
 app.use(express.json())
 
 //

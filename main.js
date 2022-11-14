d1a9e9472b29a59439ff3dc98136708887ecdfc5
Mahdymax <mahdyupwork@gmail.com>
2022-11-14 20:52:21 +0330

    normalize

--- main.js
+++ main.js
@@ -1,27 +1,18 @@
 const express = require('express');
 const app = express();
 const nodemailer = require('nodemailer');
-
-// For ( sendgrid )
-// const path = require('path');
 const sendEmail = require('./public/js/app');
-//
 
+// Deifining Port
 const PORT = process.env.PORT || 5000;
 
-
-// For ( sendgrid )
-app.use(express.urlencoded({ extended: false}));
-//
-
 // Middleware
+app.use(express.urlencoded({ extended: false}));
 app.use(express.static('/public'));
 app.use('/css', express.static(__dirname + '/public/css'))
 app.use('/js', express.static(__dirname + '/public/js'))
 app.use('/images', express.static(__dirname + '/public/images'))
 app.use(express.json())
-
-//
 
 // Set views - engine
 app.set('views', './views')
@@ -37,57 +28,20 @@ app.get('/Suggestion', (req, res) => {
 app.get('/Contact', (req, res) => {
   res.render(__dirname + '/views/Contact.ejs')
 })
-
 app.get('/suggsend', (req, res) => {
   res.render(__dirname + '/views/suggsend.ejs')
 })
-
 app.get('/suppsend', (req, res) => {
   res.render(__dirname + '/views/suppsend.ejs')
 })
 //
 
-
-
 // Send email 
-// app.post('/', (req, res) => {
-//     console.log(req.body);
-    
-//     const transporter = nodemailer.createTransport({
-//         service: 'gmail',
-//         auth:  {
-//             user: 'maxvegra@gmail.com',
-//             pass: 'pyasxnawqnlhqqya'
-//         }
-//     })
-    
-//     const mailOptions = {
-//     from: "New Report",
-//     to: 'mahdyupwork@gmail.com',
-//     subject: `New Contact Enguiry`,
-//     html: `Name: ${req.body.name} <br> 
-//     Email: ${req.body.email} <br> 
-//     Message: ${req.body.message}`
-// }
-
-// transporter.sendMail(mailOptions, (error, info) => {
-//     if(error) {
-//         console.log(error);
-//         res.send('error');
-//     }else {
-//         console.log('Email sent: ' + info.response);
-//         res.send('success')
-//     }
-// })
-// })
-//
-
-// Send email #2
 
 app.post('/sendemail', (req, res) => {
     const { name, email, message } = req.body;
 
-    const from = 'MahdyUpwork@gmail.com';
+    const from = 'lonnieraven@rrsautomotive.com';
     const to = "suggestions@ravenvending.win";
     const subject = "Suggestion Inquiry";
 
@@ -104,14 +58,13 @@ app.post('/sendemail', (req, res) => {
     `;
 
     sendEmail(to, from, subject, output);
-    // res.send('sent')
     res.redirect('/suggsend')
 }); 
 
 app.post('/sendsupport', (req, res) => {
     const { name, email, message } = req.body;
 
-    const from = 'Mahdyupwork@gmail.com';
+    const from = 'lonnieraven@rrsautomotive.com';
     const to = "support@ravenvending.win";
     const subject = "Contact Inquiry";
 
@@ -128,12 +81,11 @@ app.post('/sendsupport', (req, res) => {
     `;
 
     sendEmail(to, from, subject, output);
-    // res.send('sent')
     res.redirect('/suppsend')
 }); 
 
 
-
+// Calling the Port
 app.listen(PORT, () => {
     console.log(`Server running port ${PORT}`)
 })
